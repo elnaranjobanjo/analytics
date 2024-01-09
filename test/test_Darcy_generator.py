66e0ef7ff9d161e0dfc4cb1bb9ee58d8f76b7984
elnaranjobanjo <sebnaran@gmail.com>
2024-01-09 14:51:48 -0500

    Modernized the tests to the new api

--- test/test_Darcy_generator.py
+++ test/test_Darcy_generator.py
@@ -3,46 +3,17 @@ import fenics as fe
 import numpy as np
 import sys
 
-sys.path.append("../src/generators/")
+sys.path.append("../src/formulations/")
+sys.path.append("../src/formulations/PDEs")
+sys.path.append("../src/FEM_solvers/")
 
-import Darcy_generator as Dg
-
-
-from dolfin import *
-
-
-def solver_online(A, mesh, degree, f):
-    # Create mesh and define function space
-
-    V = FunctionSpace(mesh, "Lagrange", degree)
-
-    def boundary(x):
-        return (
-            x[0] < fe.DOLFIN_EPS
-            or x[0] > 1.0 - fe.DOLFIN_EPS
-            or x[1] < fe.DOLFIN_EPS
-            or x[1] > 1.0 - fe.DOLFIN_EPS
-        )
-
-    # Define boundary condition
-    u0 = Constant(0.0)
-    bc = DirichletBC(V, u0, boundary)
-
-    # Define variational problem
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    df = Expression(f, degree=degree)
-    a = inner(fe.Constant(A) * grad(u), grad(v)) * dx
-    L = df * v * dx
-
-    # Compute solution
-    u = Function(V)
-    solve(a == L, u, bc)
-    return u, V
+import formulation as F
+import FEM_solver as S
+import Darcy as D
 
 
 def test_dual_convergence(
-    hs: list, test_params: Dg.DarcySimParams, p_expression: str, u_expression: str
+    hs: list, test_params: F.formulation_params, p_expression: str, u_expression: str
 ) -> None:
     test_params.formulation = "dual"
     for h in hs:
@@ -52,7 +23,7 @@ def test_dual_convergence(
         )
 
         print(f"{h = }")
-        generator = Dg.Darcy_FEM_Solver(test_params)
+        generator = S.Darcy_FEM_Solver(test_params)
         print(f"{generator.formulation.get_mesh().num_cells() = }")
         (u, p) = generator.solve_variational_form(np.array([[5, 1], [1, 5]])).split()
         print(f"{len(u.vector().get_local()) = }")
@@ -95,7 +66,7 @@ def test_dual_convergence(
 
 
 def test_primal_convergence(
-    hs: list, test_params: Dg.DarcySimParams, p_expression: str
+    hs: list, test_params: F.formulation_params, p_expression: str
 ) -> None:
     test_params.formulation = "primal"
     for h in hs:
@@ -106,38 +77,25 @@ def test_primal_convergence(
 
         print(f"{h = }")
 
-        # generator = Dg.Darcy_FEM_Solver(test_params)
-        # print(f"{generator.formulation.get_mesh().num_cells() = }")
-        # p = generator.solve_variational_form(np.array([[5, 1], [1, 5]]))
-        p, V = solver_online(
-            np.array([[5, 1], [1, 5]]),
-            test_params.mesh,
-            test_params.degree,
-            test_params.f,
-        )
+        generator = S.Darcy_FEM_Solver(test_params)
+        print(f"{generator.formulation.get_mesh().num_cells() = }")
+        p = generator.solve_variational_form(np.array([[5, 1], [1, 5]]))
 
-        # plt.figure()
-        # fe.plot(p)
-        # plt.show()
-
-        # p_exact = fe.interpolate(
-        #     fe.Expression(p_expression, degree=test_params.degree),
-        #     generator.formulation.get_model_space(),
-        # )
         p_exact = fe.interpolate(
             fe.Expression(p_expression, degree=test_params.degree),
-            V,
+            generator.formulation.get_model_space(),
         )
-        fe.set_log_level(50)
-        # p_err = np.sqrt(
-        #     fe.assemble(
-        #         (p - p_exact) ** 2 * fe.dx(domain=generator.formulation.get_mesh())
-        #     )
-        # )
 
+        fe.set_log_level(50)
         p_err = np.sqrt(
-            fe.assemble((p - p_exact) ** 2 * fe.dx(domain=test_params.mesh))
+            fe.assemble(
+                (p - p_exact) ** 2 * fe.dx(domain=generator.formulation.get_mesh())
+            )
         )
+
+        # p_err = np.sqrt(
+        #     fe.assemble((p - p_exact) ** 2 * fe.dx(domain=test_params.mesh))
+        # )
 
         print(f"{p_err = }\n")
 
@@ -145,13 +103,15 @@ def test_primal_convergence(
 if __name__ == "__main__":
     hs = [0.25, 0.125, 0.0625]
     p_expression = "x[0]*(1-x[0])*x[1]*(1-x[1])"
-    test_params = Dg.DarcySimParams(
+    test_params = F.formulation_params(
+        PDE="Darcy_primal",
         degree=3,
         f="-(2*(1-2*x[0])*(1-2*x[1])-10*x[1]*(1-x[1])-10*x[0]*(1-x[0]))",
     )
 
     print("running primal formulation convergence test\n\n")
     test_primal_convergence(hs, test_params, p_expression)
+    test_params.PDE = "Darcy_dual"
     u_expression = (
         "5*x[1]*(1-x[1])*(1-2*x[0])+x[0]*(1-x[0])*(1-2*x[1])",
         "5*x[0]*(1-x[0])*(1-2*x[1])+x[1]*(1-x[1])*(1-2*x[0])",
